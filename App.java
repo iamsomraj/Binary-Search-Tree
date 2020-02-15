bee22e40de6a9bb4f6c96d647151b7a0d9d11007
iamsomraj <iamsomraj@gmail.com>
2020-02-15 20:35:05 +0530

    Update App.java

--- App.java
+++ App.java
@@ -40,6 +40,37 @@ class BinarySearchTree {
     System.out.println("\nYou have successfully inserted " + data + " into Binary Search Tree\n");
   }
 
+  public void search() {
+    if (this.root == null) {
+      System.out.println("\nThe Binary Search Tree is empty!\n");
+    } else {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("\nEnter the data to be searched : \n");
+      int data = sc.nextInt();
+      Boolean result = searchRecursively(root, data);
+      if (result) {
+        System.out.println("\nThe data " + data + " is present in Binary Search Tree\n");
+      } else {
+        System.out.println("\nThe data " + data + " is not present in Binary Search Tree\n");
+      }
+    }
+  }
+
+  public static Boolean searchRecursively(Node root, int data) {
+    if (root == null) {
+      return false;
+    }
+    if (root.data == data) {
+      return true;
+    }
+    if (data < root.data) {
+      return searchRecursively(root.left, data);
+    } else {
+      return searchRecursively(root.right, data);
+    }
+
+  }
+
   public static void displayRecursively(Node root) {
     if (root != null) {
       displayRecursively(root.left);
@@ -64,7 +95,7 @@ class App {
     Scanner sc = new Scanner(System.in);
     int ch;
     do {
-      System.out.println("\nEnter choice:\n1.Insert\n2.Display (In Order)\n3.Exit\n");
+      System.out.println("\nEnter choice:\n1.Insert\n2.Display (In Order)\n3.Search\n4.Exit\n");
       ch = sc.nextInt();
       switch (ch) {
       case 1:
@@ -74,12 +105,16 @@ class App {
         binarySearchTree.display();
         break;
       case 3:
-        ch = 3;
+        binarySearchTree.search();
+        break;
+      case 4:
+        System.out.println("\nBinary Search Tree is terminated successfully!\n");
+        ch = 4;
         break;
       default:
         System.out.println("\nUser input is invalid!\n");
         break;
       }
-    } while (ch != 3);
+    } while (ch != 4);
   }
 }
