fdc20e73fbe76e75fc649a4d39c35e035f766cd3
iamsomraj <iamsomraj@gmail.com>
2020-02-15 19:32:45 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+class Node {
+  int data;
+  Node right;
+  Node left;
+
+  Node(int data) {
+    this.data = data;
+    this.right = null;
+    this.left = null;
+  }
+
+}
+
+class BinarySearchTree {
+  Node root = null;
+
+  public static Node insertRecursively(Node root, int data) {
+    if (root == null) {
+      root = new Node(data);
+      return root;
+    }
+
+    if (data < root.data) {
+      root.left = insertRecursively(root.left, data);
+    } else {
+      root.right = insertRecursively(root.right, data);
+    }
+
+    return root;
+
+  }
+
+  public void insert() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("\nEnter the data: \n");
+    int data = sc.nextInt();
+    root = insertRecursively(root, data);
+    System.out.println("\nYou have successfully inserted " + data + " into Binary Search Tree\n");
+  }
+
+  public static void displayRecursively(Node root) {
+    if (root != null) {
+      displayRecursively(root.left);
+      System.out.println(root.data);
+      displayRecursively(root.right);
+    }
+  }
+
+  public void display() {
+    if (root == null) {
+      System.out.println("\nThe binary tree is empty! \n\n");
+    } else {
+      System.out.println("\nThe binary tree is displayed : \n");
+      displayRecursively(root);
+    }
+  }
+}
+
+class App {
+  public static void main(String[] args) {
+    BinarySearchTree binarySearchTree = new BinarySearchTree();
+    Scanner sc = new Scanner(System.in);
+    int ch;
+    do {
+      System.out.println("\nEnter choice:\n1.Insert\n2.Display (In Order)\n3.Exit\n");
+      ch = sc.nextInt();
+      switch (ch) {
+      case 1:
+        binarySearchTree.insert();
+        break;
+      case 2:
+        binarySearchTree.display();
+        break;
+      case 3:
+        ch = 3;
+        break;
+      default:
+        System.out.println("\nUser input is invalid!\n");
+        break;
+      }
+    } while (ch != 3);
+  }
+}
